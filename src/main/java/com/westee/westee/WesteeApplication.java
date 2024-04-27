982b076d55a33143c24078237352a5bf0eff4fcc
dong <904885508@qq.com>
2024-04-27 22:50:16 +0800

    first commit

---
+++ src/main/java/com/westee/westee/WesteeApplication.java
@@ -0,0 +1,13 @@
+package com.westee.westee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/westee/westee/WesteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westee.westee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WesteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
